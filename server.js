3e2fa9cecff79c86fde5bd208e3f417aae9f068c
Punit Bansal <punitbansal202@gmail.com>
2020-03-10 23:38:06 +0530

    heroku deploy app changes6

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const todos = require('./routes/todos');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 const app = express();
 // ... other imports
 const path = require('path');
@@ -18,4 +18,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`Server started on port 8000`));
+app.listen(port, () => console.log(`Server started on port` + port));
