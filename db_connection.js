8faf022a9d69aa5dcbe8e1b97dc681a3a8f229b2
Punit Bansal <punitbansal202@gmail.com>
2020-03-10 22:33:50 +0530

    heroku deploy app changes4

--- db_connection.js
+++ db_connection.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
 
 const mysql_uri =
-  process.env.MYSQL_URI || 'mysql://punit:password@192.168.0.100/todosdb';
+  process.env.MYSQL_URI ||
+  'mysql://wXtMolBUSe:jAqWjZ2Ann@remotemysql.com/wXtMolBUSe';
 const sequelize = new Sequelize(mysql_uri);
 
 sequelize
